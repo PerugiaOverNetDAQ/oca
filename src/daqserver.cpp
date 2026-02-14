18683fddf8d0722c2da89d4589fe73c089effb7f
Mattia Barbanera <mattia.barbanera@pg.infn.it>
2026-02-14 16:47:41 +0100

    Temporarily revert behavior without trigger board

--- src/daqserver.cpp
+++ src/daqserver.cpp
@@ -158,10 +158,10 @@ void daqserver::SelectTrigger(uint32_t trig){
 
   trigtype = trig;
   
-  det[portdet.size()-1]->SelectTrigger(trigtype);
-  //for (int ii=0; ii<(int)(det.size()); ii++) {
-  //  det[ii]->SelectTrigger(trigtype);
-  //}
+  //det[portdet.size()-1]->SelectTrigger(trigtype);
+  for (int ii=0; ii<(int)(det.size()); ii++) {
+    det[ii]->SelectTrigger(trigtype);
+  }
 
   return;
 }
