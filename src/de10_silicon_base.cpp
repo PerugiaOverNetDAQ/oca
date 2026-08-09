97cfc276cb5933011d14748543a68ab6d507e35b
Mattia Barbanera <mattia.barbanera@pg.infn.it>
2026-08-09 17:26:54 +0200

    Fix Bias Voltage conversion factor; add SetConfigBias at run start

--- src/de10_silicon_base.cpp
+++ src/de10_silicon_base.cpp
@@ -575,7 +575,6 @@ int de10_silicon_base::SetBias(string _bias0, string _bias1){
   else {
     ret = 1;
   }
-
   ret += checkReply("Setting Biases");
   
   return ret;
@@ -600,6 +599,10 @@ int de10_silicon_base::GetBiasCurr(float& _curr0, float& _curr1, uint8_t& _flags
 
 int de10_silicon_base::runStart() {
   int ret = 0;
+  
+  if (SetConfigBias()!=0) {
+    ret = 1;
+  }
   if (SendCmd("runStart")!=0) {
     ret = 1;
   }
